8e497958f590d5f73536e1601953777bb1d3ff51
AndreaColinsR <44866486+AndreaColinsR@users.noreply.github.com>
2026-04-20 21:00:29 -0400

    pipeline

---
+++ .ipynb_checkpoints/bodypose3d-checkpoint.py
@@ -0,0 +1,151 @@
+import cv2 as cv
+import mediapipe as mp
+import numpy as np
+import sys
+from utils import DLT, write_keypoints_to_disk
+#add here if you need more keypoints
+
+
+#this will load the sample videos if no camera ID is given
+Nvideo = '8'
+Calib_n = '7'
+
+Video_nameA = '.\Videos\pcte'+Nvideo+'A.avi'
+Video_nameB = '.\Videos\pcte'+Nvideo+'B.avi'
+input_stream1 = Video_nameA
+input_stream2 = Video_nameB
+print(input_stream1)
+
+pose_keypoints = [16, 14, 12, 11, 13, 15, 24, 23, 25, 26, 27, 28]
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+
+Calib_video = '.\Calibration_Videos_RS\Calibration_parameters_'+Calib_n+'.npz'
+npz = np.load(Calib_video)
+
+
+P0=npz['P1']
+P1=npz['P2']
+R=npz['R']
+T=npz['T']
+
+
+#create body keypoints detector objects.
+#INFO: Created TensorFlow Lite XNNPACK delegate for CPU.
+pose0 = mp_pose.Pose(min_detection_confidence=0.8, min_tracking_confidence=0.8,model_complexity=2)
+pose1 = mp_pose.Pose(min_detection_confidence=0.8, min_tracking_confidence=0.8,model_complexity=2)
+
+#containers for detected keypoints for each camera. These are filled at each frame.
+#This will run you into memory issue if you run the program without stop
+kpts_cam0 = []
+kpts_cam1 = []
+kpts_3d = []
+
+## Start reading the videos
+cap0 = cv.VideoCapture(input_stream1)
+cap1 = cv.VideoCapture(input_stream2)
+caps = [cap0, cap1]
+
+
+while(cap0.isOpened()):
+    ret0, frame0 = cap0.read()
+    ret1, frame1 = cap1.read()
+    if not ret0 or not ret1: 
+        break
+    else:
+        frame0 = cv.cvtColor(frame0, cv.COLOR_BGR2RGB)
+        frame1 = cv.cvtColor(frame1, cv.COLOR_BGR2RGB)
+        
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        frame0.flags.writeable = False
+        frame1.flags.writeable = False
+        results0 = pose0.process(frame0)
+        results1 = pose1.process(frame1)
+    
+        #reverse changes
+        frame0.flags.writeable = True
+        frame1.flags.writeable = True
+        frame0 = cv.cvtColor(frame0, cv.COLOR_RGB2BGR)
+        frame1 = cv.cvtColor(frame1, cv.COLOR_RGB2BGR)
+    
+        #check for keypoints detection
+        frame0_keypoints = []
+        
+        if results0.pose_landmarks:
+            for i, landmark in enumerate(results0.pose_landmarks.landmark):
+                if i not in pose_keypoints: continue #only save keypoints that are indicated in pose_keypoints
+                    
+                pxl_x = landmark.x * frame0.shape[1]
+                pxl_y = landmark.y * frame0.shape[0]
+                pxl_x = int(round(pxl_x))
+                pxl_y = int(round(pxl_y))
+                cv.circle(frame0,(pxl_x, pxl_y), 3, (0,0,255), -1) #add keypoint detection points into figure
+                kpts = [pxl_x, pxl_y]
+                frame0_keypoints.append(kpts) ## clearly the problem is around the next line 
+        else:
+            #if no keypoints are found, simply fill the frame data with [-1,-1] for each kpt
+            frame0_keypoints = [[-1, -1]]*len(pose_keypoints)
+                
+        #this will keep keypoints of this frame in memory
+        kpts_cam0.append(frame0_keypoints)
+
+        ## For the second camera
+        frame1_keypoints = []
+        if results1.pose_landmarks:
+            for i, landmark in enumerate(results1.pose_landmarks.landmark):
+                    
+                if i not in pose_keypoints: continue
+                        
+                pxl_x = landmark.x * frame1.shape[1]
+                pxl_y = landmark.y * frame1.shape[0]
+                pxl_x = int(round(pxl_x))
+                pxl_y = int(round(pxl_y))
+                cv.circle(frame1,(pxl_x, pxl_y), 3, (0,0,255), -1)
+                kpts = [pxl_x, pxl_y]
+                frame1_keypoints.append(kpts)
+    
+        else:
+            #if no keypoints are found, simply fill the frame data with [-1,-1] for each kpt
+            frame1_keypoints = [[-1, -1]]*len(pose_keypoints)
+    
+        #update keypoints container
+        kpts_cam1.append(frame1_keypoints)
+        #print(frame1_keypoints)
+        
+        #calculate 3d position
+        frame_p3ds = []
+        for uv1, uv2 in zip(frame0_keypoints, frame1_keypoints):
+            if uv1[0] == -1 or uv2[0] == -1:
+                _p3d = [-1, -1, -1]
+            else:
+                _p3d = DLT(P0, P1, uv1, uv2) #calculate 3d position of keypoint
+            frame_p3ds.append(_p3d)
+                
+        frame_p3ds = np.array(frame_p3ds).reshape((12, 3))
+        kpts_3d.append(frame_p3ds)
+        # uncomment these if you want to see the full keypoints detections
+        mp_drawing.draw_landmarks(frame0, results0.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        mp_drawing.draw_landmarks(frame1, results1.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # Show reults
+        frame_cat = cv.hconcat([frame0,frame1])
+        half = cv.resize(frame_cat, (0, 0), fx = 0.5, fy = 0.5)
+        cv.imshow('cam0', half)
+      
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break #27 is ESC key.
+    
+for cap in caps:
+    cap.release()
+cv.destroyAllWindows()
+
+#this will create keypoints file in current working folder
+write_keypoints_to_disk('.\Tracking\kpts_cam0_'+Nvideo+'.dat', kpts_cam0)
+write_keypoints_to_disk('.\Tracking\kpts_cam1_'+Nvideo+'.dat', kpts_cam1)
+write_keypoints_to_disk('.\Tracking\kpts_3d_'+Nvideo+'.dat', kpts_3d)
+print('All done')
---
+++ .ipynb_checkpoints/show_3d_pose-checkpoint.py
@@ -0,0 +1,222 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import DLT
+import cv2 as cv
+from scipy.io import savemat
+plt.style.use('seaborn-v0_8')
+
+
+pose_keypoints = np.array([16, 14, 12, 11, 13, 15, 24, 23, 25, 26, 27, 28])
+
+def read_keypoints(filename):
+    fin = open(filename, 'r')
+
+    kpts = []
+    while(True):
+        line = fin.readline()
+        if line == '': break
+
+        line = line.split()
+        line = [float(s) for s in line]
+
+        line = np.reshape(line, (len(pose_keypoints), -1))
+        kpts.append(line)
+
+    kpts = np.array(kpts)
+    return kpts
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector."""
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    """Finds angle between two vectors"""
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+def x_rotation(vector,theta):
+    """Rotates 3-D vector around x-axis"""
+    R = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0, np.sin(theta), np.cos(theta)]])
+    return np.dot(R,vector)
+
+def y_rotation(vector,theta):
+    """Rotates 3-D vector around y-axis"""
+    R = np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta), 0, np.cos(theta)]])
+    return np.dot(R,vector)
+
+def z_rotation(vector,theta):
+    """Rotates 3-D vector around z-axis"""
+    R = np.array([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
+    return np.dot(R,vector)
+    
+def visualize_3d(p3ds,p3dsf,capF,capS,t):
+
+    """Now visualize in 3D"""
+    torso = [[0, 1] , [1, 7], [7, 6], [6, 0]]
+    armr = [[1, 3], [3, 5]]
+    arml = [[0, 2], [2, 4]]
+    legr = [[6, 8], [8, 10]]
+    legl = [[7, 9], [9, 11]]
+    body = [torso, arml, armr, legr, legl]
+    colors = ['red', 'blue', 'green', 'black', 'orange']
+
+
+    counter=1
+    tmp=p3ds.shape
+    print(p3ds.shape)
+    Nframes=tmp[0]
+    t=t[0:Nframes]
+
+    ## Translate skeleton to the origin of the system
+    p3ds[0,:,:]=p3ds[1,:,:]
+
+    tmp_nan=[];
+    for i in np.arange(0,Nframes):
+        if np.sum(p3ds[i,0,:])>-3.1 and np.sum(p3ds[i,0,:])<-2.9:
+            tmp_nan.append(i)
+            
+    p3ds=np.delete(p3ds,tmp_nan, axis=0)
+    t = np.delete(t, tmp_nan)
+    
+    p3ds = p3ds-p3ds[0,10,:]
+    
+    ## commpute left knee angle
+    #6-8 vs 8-10
+    
+    tmp=p3ds.shape
+    Nframes=tmp[0]
+    l_knee=np.zeros((Nframes,))
+    r_knee=np.zeros((Nframes,))
+
+    l_hip=np.zeros((Nframes,))
+    r_hip=np.zeros((Nframes,))
+    
+    for i in np.arange(0,Nframes):
+            
+        l_knee[i]=angle_between(p3ds[i,6,:]-p3ds[i,8,:], p3ds[i,8,:]-p3ds[i,10,:])
+        print(t[i],l_knee[i])
+        r_knee[i]=angle_between(p3ds[i,7,:]-p3ds[i,9,:], p3ds[i,9,:]-p3ds[i,11,:])
+
+        l_hip[i]=angle_between(p3ds[i,0,:]-p3ds[i,6,:], p3ds[i,6,:]-p3ds[i,8,:])
+        r_hip[i]=angle_between(p3ds[i,1,:]-p3ds[i,7,:], p3ds[i,7,:]-p3ds[i,9,:])
+
+    fig0 = plt.figure()
+    plt.subplot(211)
+    plt.plot(t[1:len(t)],r_hip[1:len(t)]*180/np.pi,'r',label='Rodilla derecha')
+    plt.plot(t[1:len(t)],l_hip[1:len(t)]*180/np.pi,'k',label='Rodilla izquierda')
+    
+    plt.subplot(212)
+    plt.plot(t[1:len(t)],r_knee[1:len(t)]*180/np.pi,'r',label='Rodilla derecha')
+    plt.plot(t[1:len(t)],l_knee[1:len(t)]*180/np.pi,'k',label='Rodilla izquierda')
+    plt.xlabel('Tiempo [s]')
+    plt.ylabel('Angulo [o]')
+    plt.legend(loc="upper left")
+    plt.ylim(0, 150)
+
+    
+    #plt.plot(t,p3ds[:,10,0],'b')
+    #plt.plot(t,p3ds[:,10,1],'g')
+    #plt.plot(t,p3ds[:,10,2],'r')
+    plt.pause(1)
+    
+
+
+
+    from mpl_toolkits.mplot3d import Axes3D
+    
+    fig = plt.figure()
+    axF = fig.add_subplot(221)
+    axS = fig.add_subplot(222)
+    ax = fig.add_subplot(223, projection='3d')
+    ax2 = fig.add_subplot(224, projection='3d')
+
+    
+    #new_z = np.zeros((2,3))
+    new_z = ((p3ds[0,0,:]-p3ds[0,10,:])+(p3ds[0,1,:]-p3ds[0,11,:]))/2
+    alpha_z = angle_between(new_z, np.array([0.0, 0.0, 1.0]))
+    new_z = z_rotation(new_z, -alpha_z)
+
+    
+    Mins = np.min(np.min(p3ds,0),0)-100
+    Maxs = np.max(np.max(p3ds,0),0)+100
+
+    for framenum, kpts3d in enumerate(p3ds):
+        ret, frameF = capF.read()
+        ret, frameS = capS.read()
+        if framenum%2 == 0: continue #skip every 2nd frame
+        axF.imshow(cv.cvtColor(frameF, cv.COLOR_BGR2RGB))
+        axF.axis('off')
+        axS.imshow(cv.cvtColor(frameS, cv.COLOR_BGR2RGB))
+        axS.axis('off')
+
+        ax.plot(np.linspace(0,new_z[0]),np.linspace(0,new_z[1]),np.linspace(0,new_z[2]))
+        #ax.plot(np.linspace(0,new_vect[0]),np.linspace(0,new_vect[1]),np.linspace(0,new_vect[2]))
+
+
+        for bodypart, part_color in zip(body, colors):
+            for _c in bodypart:
+                ax.plot(xs = [kpts3d[_c[0],0], kpts3d[_c[1],0]], ys = [kpts3d[_c[0],1], kpts3d[_c[1],1]], zs = [kpts3d[_c[0],2], kpts3d[_c[1],2]], linewidth = 4, c = part_color)
+                ax2.plot(xs = [kpts3d[_c[0],0], kpts3d[_c[1],0]], ys = [kpts3d[_c[0],1], kpts3d[_c[1],1]], zs = [kpts3d[_c[0],2], kpts3d[_c[1],2]], linewidth = 4, c = part_color)
+
+        #uncomment these if you want scatter plot of keypoints and their indices.
+        for i in range(12):
+            ax.scatter(xs = kpts3d[i:i+1,0], ys = kpts3d[i:i+1,1], zs = kpts3d[i:i+1,2])
+            #ax2.text(kpts3d[i,0], kpts3d[i,1], kpts3d[i,2], str(i))
+            ax2.scatter(xs = kpts3d[i:i+1,0], ys = kpts3d[i:i+1,1], zs = kpts3d[i:i+1,2])
+        
+        #for _c in connections:
+           # ax.plot(xs = [p3dsf[_c[0],0], p3dsf[_c[1],0]], ys = [p3dsf[_c[0],1], p3dsf[_c[1],1]], zs = [p3dsf[_c[0],2], p3dsf[_c[1],2]], c = 'red')
+           # ax2.plot(xs = [p3dsf[_c[0],0], p3dsf[_c[1],0]], ys = [p3dsf[_c[0],1], p3dsf[_c[1],1]], zs = [p3dsf[_c[0],2], p3dsf[_c[1],2]], c = 'red')
+
+        #ax.set_axis_off()
+        #ax2.set_axis_off()
+        ####################### right plot (front)
+        ax2.set_xlim3d([Mins[0], Maxs[0]])
+        ax2.set_ylim3d([Mins[1], Maxs[1]])
+        ax2.set_zlim3d([Mins[2], Maxs[2]])
+        ax2.view_init(elev=-38, azim=-91,roll=40)
+        #ax2.view_init(elev=-0, azim=0,roll=-110)
+        #ax.set_xticks([])
+        #ax.set_yticks([])
+        #ax.set_zticks([])
+        ####################### Left plot (side)
+        ax.set_xlim3d([Mins[0], Maxs[0]])
+        ax.set_ylim3d([Mins[1], Maxs[1]])
+        ax.set_zlim3d([Mins[2], Maxs[2]])
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        
+        ax.view_init(elev=-163, azim=0,roll=-90)
+        plt.title(framenum)
+        figname='.\Frames\Fig_'+str(counter)+'.png'
+        plt.savefig(figname, bbox_inches='tight')
+        plt.pause(0.005)
+        if framenum==1:
+            plt.pause(0.05)
+        ax.cla()
+        ax2.cla()
+        axF.cla()
+        axS.cla()
+        
+        counter=counter+1
+
+if __name__ == '__main__':
+    Nvideo = '8'
+    capF = cv.VideoCapture(r'.\Videos\pcte'+Nvideo+'A.avi')
+    capS = cv.VideoCapture(r'.\Videos\pcte'+Nvideo+'B.avi')
+    
+    #npz = np.load('Floor_points.npz')
+    #connections=npz['connections']
+    #p3dsf=npz['p3dsf']
+    npz = np.load(r'.\Videos\pcte'+Nvideo+'_t.npz')
+    t = npz['t']
+    p3ds = read_keypoints('.\Tracking\kpts_3d_'+Nvideo+'.dat')
+    p3dsf = read_keypoints('.\Tracking\kpts_3d_'+Nvideo+'.dat')
+    mdic = {"body_pose": p3ds,"t":t}
+    savemat(".\pose_3d_video_"+Nvideo+".mat", mdic)
+    p3ds
+    visualize_3d(p3ds,p3dsf,capF,capS,t)
+    capF.release() 
+    capS.release()
